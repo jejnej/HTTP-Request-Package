248e588654338d69c2cf153127c6dbe71cbdb899
Jojo <jody.sekhon@gmail.com>
2018-03-19 18:48:19 +0000

    HTTP requests using the request package. Retrieving image future from https://sytantris.github.io/http-examples/future.jpg

---
+++ request.js
@@ -0,0 +1,24 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+
+       .on('error', function (err) {
+         throw err;
+       })
+
+       .on('response', function (response) {
+        console.log('Downloading image...');
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Status Message: ', response.statusMessage);
+         console.log('Response Content Type: ', response.headers['content-type']);
+
+       })
+       .on('end', function() {
+         console.log('Download complete.')
+       })
+       .pipe(fs.createWriteStream('./future.jpg'));
+
+
+
+
